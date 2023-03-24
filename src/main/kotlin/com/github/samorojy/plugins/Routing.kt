6c313483ef88247b30c2c1c532ca288c8e1b53cb
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2023-03-24 20:02:28 +0200

    added routings

--- src/main/kotlin/com/github/samorojy/plugins/Routing.kt
+++ src/main/kotlin/com/github/samorojy/plugins/Routing.kt
@@ -1,13 +1,35 @@
 package com.github.samorojy.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+
+suspend fun executeChecker(code: String): String = withContext(Dispatchers.IO) {
+    val process = ProcessBuilder(
+        "python3", "plagiarism_checker.py", code
+    ).directory(File("src/main/python/com/github/samorojy/")).start()
+    process.waitFor()
+    String(process.inputStream.readBytes()).trim()
+}
 
 fun Application.configureRouting() {
+
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Hello World! I am an extraordinary code checker for similarity!")
+        }
+    }
+
+    routing {
+        post("/check") {
+            // It can be a json or any other query body. But for simplicity, the full body is used.
+            val requestBody = call.receive<String>()
+            val output = executeChecker(requestBody)
+            call.respondText(output)
         }
     }
 }
--- src/test/kotlin/com/github/samorojy/ApplicationTest.kt
+++ src/test/kotlin/com/github/samorojy/ApplicationTest.kt
@@ -1,5 +1,37 @@
 package com.github.samorojy
 
+import com.github.samorojy.plugins.executeChecker
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 
 class ApplicationTest {
+    // can only be launched with the database running
+    // Also there could be the tokenization tests but pygmentize returns tokens in random order
+    /*@Test
+    fun plagiarismTrueTest() {
+        val codeToCheck = javaClass.classLoader.getResource("KtConfigurator.txt")?.readText()
+        val correctAnswer =
+            """educational-plugin-master\educational-plugin-master\Edu-Kotlin\src\com\jetbrains\edu\kotlin\KtConfigurator.kt"""
+        codeToCheck?.let {
+            runBlocking {
+                assertEquals(correctAnswer, executeChecker(it))
+            }
+        }
+    }
+
+    @Test
+    fun plagiarismFalseTest() {
+        val codeToCheck = javaClass.classLoader.getResource("PyHelloWorld.txt")?.readText()
+        val correctAnswer = "OK"
+        codeToCheck?.let {
+            runBlocking {
+                assertEquals(correctAnswer, executeChecker(it))
+            }
+        }
+    }*/
 }
